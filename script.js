fba90991b82b8760567441d3ed0d82d0d4325dda
hazratbilal <hazratbilal52521@gmail.com>
2024-01-22 16:36:14 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let display = document.querySelector("#textdisplay");
+
+function displayAppend(value){
+    display.value += value;
+}
+
+function clearDis() {
+    display.value = '';
+}
+
+function calculate() {
+    let calculate = display.value
+
+
+    if(calculate.includes("/0")){
+        display.value = "Error"
+    } else {
+        display.value = eval(display.value)
+    }
+}
+
+
+
+
